0533780e84061bc7ded2854443ce2aab494bbf31
Eric Tonussi <erictonussi@gmail.com>
2017-09-26 11:22:37 -0300

    enviando compras para o sankhya

--- app/code/local/Xyz/Catalog/Model/Price/Observer.php
+++ app/code/local/Xyz/Catalog/Model/Price/Observer.php
@@ -1,4 +1,8 @@
 <?php
+
+include '/api/sankhya.php';
+
+
 class Xyz_Catalog_Model_Price_Observer
 {
     public function __construct()
@@ -9,11 +13,76 @@ class Xyz_Catalog_Model_Price_Observer
      * @param   Varien_Event_Observer $observer
      * @return  Xyz_Catalog_Model_Price_Observer
      */
-    public function apply_discount_percent($observer)
+    public function orderExport($observer)
     {
-      $event = $observer->getEvent();
-         $model = $event->getPage();
-   	     // print_r($model->getData());
-         // die('test');
+        $order = $observer->getEvent()->getOrder();
+        $payment = $order->getPayment();
+
+        $orderId = $order->getIncrementId();
+
+
+        $parceiro = $order->getCustomerId();
+
+        switch ($payment->getMethodInstance()->getCode()) {
+            case 'boletopayment':
+                # code...
+                $tipo_venda = $payment->getBoletoSankhyaId();
+                break;
+
+            default:
+                # code...
+                break;
+        }
+
+        $items = array();
+
+        // foreach ($order->getAllItems() as $item) {
+        foreach ($order->getAllVisibleItems() as $item) {
+          // $prod_opts = $item->getData('product_options');
+          // $prod_opts = unserialize($prod_opts);
+
+          // // var_dump($prod_opts); die();
+
+          // foreach($prod_opts['options'] as $opt){
+          //    $opt_value = $opt['value'];
+          //    echo "\n$opt_value";
+          // }
+
+          $items[] = array(
+              'id'            => Mage::getModel('catalog/product')->load($item->getProductId())->getsankhya_id(),
+              // 'name'          => $item->getName(),
+              // 'sku'           => $item->getSku(),
+              'valor'         => $item->getPrice(),
+              'qtd'   => $item->getQtyOrdered(),
+          );
+        }
+
+        // var_dump($items); die();
+
+        $sankhya = new Sankhya();
+
+        echo "parceiro: $parceiro, tipo_venda: $tipo_venda: \n";
+
+        $parsed = $sankhya->criar_nota($parceiro, $tipo_venda);
+        var_dump($parsed);
+
+        $nota = (string)$parsed->responseBody->pk->NUNOTA;
+        // $nota = 50181;
+
+        $parsed = $sankhya->incluir_item_nota($nota, $items);
+        var_dump($parsed);
+
+        $parsed = $sankhya->confirmar_nota($nota);
+        // var_dump($parsed);
+
+
+
+        // $customerData = Mage::getModel('customer/customer')->load($customer_id);
+        // $customerName = $customerData->getName();
+
+
+        // echo $orderId;
+        // $payment->_toHtml();
+        die('test');
     }
 }
